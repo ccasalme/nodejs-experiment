f2098ed98c9c54f7d432fe686d6ad2ea4400f4c5
ccasalme <78170262+ccasalme@users.noreply.github.com>
2024-12-09 18:47:08 -0500

    npm

--- app.js
+++ app.js
@@ -1,50 +1,61 @@
-// // // // // //JS experiment
+// // // // // // //JS experiment
 
-// // // // // function sayHello(name){
-// // // // //     console.log("Hello " + name);
-// // // // // }
-// // // // // //sayHello("cyrl");
+// // // // // // function sayHello(name){
+// // // // // //     console.log("Hello " + name);
+// // // // // // }
+// // // // // // //sayHello("cyrl");
 
-// // // // // // console.log(window);
+// // // // // // // console.log(window);
 
-// // // // console.log(); //global object
-// // // // setTimeout(); //global object
-// // // // clearTimeout(); //global object
+// // // // // console.log(); //global object
+// // // // // setTimeout(); //global object
+// // // // // clearTimeout(); //global object
 
-// // // // window.console.log(); //global object
-// // // // var message = ''; //global object
-// // // // // however, functions that are defined in var message = ''; are not global objects
-// // // // global.console.log(); //global object
+// // // // // window.console.log(); //global object
+// // // // // var message = ''; //global object
+// // // // // // however, functions that are defined in var message = ''; are not global objects
+// // // // // global.console.log(); //global object
 
-// // // //console.log(module);
+// // // // //console.log(module);
 
-// // // const logger = require('./logger');
-// // // // console.log(logger);
-// // // logger.log('message');
+// // // // const logger = require('./logger');
+// // // // // console.log(logger);
+// // // // logger.log('message');
 
-// // // instead of exporting an object from a module, we can export a single function
+// // // // instead of exporting an object from a module, we can export a single function
 
-// // // require('path');
+// // // // require('path');
 
-// // // const pathObj = path.parse(__filename);
-// // // console.log(pathObj);
+// // // // const pathObj = path.parse(__filename);
+// // // // console.log(pathObj);
 
-// // const os = require('node:os');
+// // // const os = require('node:os');
 
-// // const totalMemory = os.totalmem();
-// // const freeMemory = os.freemem();
-// // // console.log('Total Memory: ' + totalMemory);
-// // console.log(`Total Memory: ${totalMemory}`);
-// // console.log(`Free Memory: ${freeMemory}`);
+// // // const totalMemory = os.totalmem();
+// // // const freeMemory = os.freemem();
+// // // // console.log('Total Memory: ' + totalMemory);
+// // // console.log(`Total Memory: ${totalMemory}`);
+// // // console.log(`Free Memory: ${freeMemory}`);
 
-// const fs = require('fs');
+// // const fs = require('fs');
 
-// // const files = fs.readdirSync('./');
-// // console.log(files);
+// // // const files = fs.readdirSync('./');
+// // // console.log(files);
 
-// fs.readdir('./', function(err, files){
-//     if (err) console.log('Error', err);
-//     else console.log('Result', files);
+// // fs.readdir('./', function(err, files){
+// //     if (err) console.log('Error', err);
+// //     else console.log('Result', files);
+// // });
+
+// // // const EventEmitter = require('events');
+
+// const EventEmitter = require('node:events'); //class
+// const emitter = new EventEmitter(); //object
+
+// //register a listener
+// emitter.on('messageLogged', (arg) => {
+//     console.log('Listener called', arg);
 // });
 
-// // const EventEmitter = require('events');
+// emitter.emit('messageLogged', {id: 1, url: 'http://'}); //raise an event
+
