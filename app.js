f1f1b32191ce8f7b217617b720246eab8d996a30
ccasalme <78170262+ccasalme@users.noreply.github.com>
2024-12-09 18:51:28 -0500

    app.js readme.md

--- app.js
+++ app.js
@@ -59,3 +59,101 @@
 
 // emitter.emit('messageLogged', {id: 1, url: 'http://'}); //raise an event
 
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description of your project:'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How is your project installed?'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How is your project used?'
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'How can others contribute to your project?'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'How is your project tested?'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPLv3', 'BSD 3-Clause', 'None']
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username:'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:'
+    }
+  ]);
+}
+
+function generateREADME(answers) {
+  return `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Questions
+For questions, please contact me at:
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: ${answers.email}
+`;
+}
+
+promptUser().then((answers) => {
+  const readmeContent = generateREADME(answers);
+  fs.writeFileSync('README.md', readmeContent);
+  console.log('README.md generated successfully!');
+});
+
